5204cf908cfd73822968ebdb16e1aacbc352174f
psychokino <48856527+psychokino@users.noreply.github.com>
2019-04-30 21:51:36 +0800

    Add files via upload

---
+++ RSA/rsa-decrypt.cpp
@@ -0,0 +1,107 @@
+﻿
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "pch.h"
+#include <bits/stdc++.h>
+#include <string>
+#include <algorithm>
+#include "../../function_lib/cryptopp810/files.h"
+#include "../../function_lib/cryptopp810/filters.h"
+#include "../../function_lib/cryptopp810/modes.h"
+#include "../../function_lib/cryptopp810/hex.h"
+#include "../../function_lib/cryptopp810/randpool.h"
+#include "../../function_lib/cryptopp810/sha.h"
+#include "../../function_lib/cryptopp810/nbtheory.h"
+#include "../../function_lib/cryptopp810/rsa.h"
+#include "../../function_lib/cryptopp810/osrng.h"
+
+using namespace CryptoPP;
+using namespace std;
+
+char hex_to_char(char a ,char b) {
+	string table = "0123456789abcdef";
+	int x;
+	x = table.find_first_of(a) * 16 + table.find_first_of(b);
+	char c = x;
+	return c;
+
+}
+
+void Myrsa_decryption(string cipher_message ,Integer private_d ,Integer product_n, Integer public_e, ofstream& ofs) {
+
+	InvertibleRSAFunction myrsa;
+	//RandomNumberGenerator rng;
+	AutoSeededRandomPool prng;
+	//cout << "bit: "<< dec << product_n.BitCount() << endl; 
+	//cout << "n  : " << hex << product_n << endl;
+	myrsa.Initialize(product_n, public_e, private_d);
+	size_t sizeof_n = product_n.BitCount(); // in ciphertext two digit form a byte , so there are total bit/4 digit needed
+	sizeof_n /= 4;
+
+
+	for ( string::iterator it = cipher_message.begin() ;   (it+2) != cipher_message.end()  ; it = cipher_message.begin() ) {
+
+		//unsigned int x = std::min(sizeof_n, cipher_message.size() ) ;  // gurantee the length mo more than key size until string input end.  
+		Integer message( cipher_message.data() );
+		//Integer message("0x194d5cdc0ec8efbc");
+		cout << " now message : " << cipher_message << endl;
+		cout << " transformed : " << hex << message << endl;
+		Integer plaintext = myrsa.CalculateInverse(prng , message);
+		cout << "plaintext : " << hex << plaintext << endl;
+		//ofs << "plaintext : " << hex << plaintext << endl;
+		
+		stringstream ss;
+		string s;
+		ss << hex << plaintext;
+		ss >> s;
+		s.erase(s.end() - 1);
+		cout << "message : ";
+		for (int i = 0; i < s.size(); i += 2) {
+			char c = hex_to_char(s[i], s[i + 1]);
+			cout << c;
+			ofs << c;
+		}
+		//cout << "message : " << s << endl;
+		cout << endl;
+		//ofs << s;
+		cipher_message.erase( 2 , sizeof_n );
+
+	}
+
+	ofs << endl;
+
+	
+
+}
+
+int main() {
+
+	ofstream ofs("output.txt");
+	/*
+		Integer type construction 
+		remember to add 0x before the value
+		or you will get the wrong key and block length.
+	*/
+	Integer e("0x11");
+
+	Integer d64("0x111242af5740d14d");
+	Integer d128("0x974f3eaa763ad0979644dbfaac47867bd87b4c5c8b7fcd72943d0dde4303639");
+
+	Integer n64("0xae20a831558c0d69");
+	Integer n128("0xa0c432951d9e7da10fa929ba570bfee52db56fc477e60b742581a35d1723ad6f"); 
+	
+	string cipher64 = "0x194d5cdc0ec8efbc" ;
+
+	string cipher128 = "0x404ea0a1c26fc6562ff17a61849520e0fdf70654c6460b0954918e8447d6cdba";
+
+	//Myrsa_decryption(cipher64, d64, n64, e, ofs);
+	Myrsa_decryption(cipher128, d128, n128, e, ofs);
+	
+
+	return 0;
+
+}
+
+
---
+++ RSA/rsa-encrypt.cpp
@@ -0,0 +1,111 @@
+﻿
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "pch.h"
+
+#include <bits/stdc++.h>
+#include <string>
+#include <algorithm>
+#include "../../function_lib/cryptopp810/files.h"
+#include "../../function_lib/cryptopp810/filters.h"
+#include "../../function_lib/cryptopp810/modes.h"
+#include "../../function_lib/cryptopp810/hex.h"
+#include "../../function_lib/cryptopp810/randpool.h"
+#include "../../function_lib/cryptopp810/sha.h"
+#include "../../function_lib/cryptopp810/nbtheory.h"
+#include "../../function_lib/cryptopp810/rsa.h"
+#include "../../function_lib/cryptopp810/osrng.h"
+
+using namespace CryptoPP;
+using namespace std;
+
+
+void Myrsa_encryption(string input_message ,Integer public_e ,Integer product_n, ofstream& ofs) {
+
+	RSAFunction myrsa;
+	//cout << "bit: "<< dec << product_n.BitCount() << endl; 
+	//cout << "n  : " << hex << product_n << endl;
+	myrsa.Initialize(product_n, public_e);
+	size_t sizeof_n = product_n.ByteCount();
+	sizeof_n;
+	product_n;
+	for ( string::iterator it = input_message.begin() ;   it != input_message.end()  ; it = input_message.begin() ) {
+
+		unsigned int x = std::min(sizeof_n, input_message.size() ) ;  // gurantee the length mo more than key size until string input end.  
+		Integer message( (const byte *)input_message.data(),  x );
+		cout << " now message : " << input_message << endl;
+		//cout << " transformed : " << hex << message << endl;
+		Integer cipher = myrsa.ApplyFunction(message);
+		
+		stringstream ss;
+		string s;
+		ss << hex << cipher;
+		ss >> s;
+		s.erase(s.end() - 1);
+		cout << "cipher : " << s << endl;
+		ofs << s;
+		input_message.erase( 0 , sizeof_n );
+
+	}
+
+	ofs << endl;
+
+	
+
+}
+
+int main() {
+
+	ofstream ofs("output.txt");
+	/*
+		Integer type construction 
+		remember to add 0x before the value
+		or you will get the wrong key and block length.
+	*/
+	Integer e("0x11");
+	Integer n64("0xab9df7c82818bab3");
+	Integer n128("0xcebe9e0617c706c632e64c3405cda5d1"); 
+	Integer n256("0xaf195de7988cfaa1dbb18c5862e3853f0e79a12bbfa7aa326a52da97caa60c39");
+	string message64 = "Alice" ;
+	string message128 = "Hello World!";
+	string message256 = "RSA is publickey.";
+
+	
+	//Myrsa_encryption(message64, e, n64, ofs);
+	Myrsa_encryption(message128, e, n128, ofs);
+	Myrsa_encryption(message256, e, n256, ofs);
+	/*
+	Integer m64((const byte *)message64.data(), message64.size());
+	cout << "m: " << hex << m64 << endl;
+	cout << "n: " << hex << n64 << endl;
+	myrsa.Initialize(n64, e);
+	cout << "valid: " << myrsa.PreimageBound() << endl;
+	Integer c64 = myrsa.ApplyFunction(m64);
+	cout << "c: " << hex << c64 << endl;
+	//ofs << "c: " << hex << c64 << endl;
+
+
+	Integer m128((const byte *)message128.data(), message128.size());
+	cout << "m: " << hex << m128 << endl;
+	cout << "n: " << hex << n128 << endl;
+	myrsa.Initialize(n128, e);
+	Integer c128 = myrsa.ApplyFunction(m128);
+	cout << "c: " << hex << c128 << endl;
+	//ofs << "c: " << hex << c128 << endl;
+
+	Integer m256((const byte *)message256.data(), message256.size());
+	cout << "m: " << hex << m256 << endl;
+	cout << "n: " << hex << n256 << endl;
+	myrsa.Initialize(n256, e);
+	Integer c256 = myrsa.ApplyFunction(m256);
+	cout << "c: " << hex << c256 << endl;
+	//ofs << "c: " << hex << c256 << endl;*/
+
+
+
+	return 0;
+
+}
+
+
